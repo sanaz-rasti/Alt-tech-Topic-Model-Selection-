f9fb5a3401c95c761dc1cf15e4f93dd5d5a52204
Sanaz Rasti <sanaz-rasti@Sanazs-MacBook-Pro.local>
2022-09-15 20:18:05 +0100

    initial

--- Data-Preprocessing/README.md
+++ Data-Preprocessing/README.md
@@ -1,1 +1,95 @@
-![DataCleaning](dc.png)
+
+
+# Description
+
+The original text corpus of our project is preprocessed using the script data_preprocessing.py. 
+
+### ---------------------------------------------
+### Reading Data
+The script includes **ChannelData** class for reading the corpus from our database. 
+
+The following steps shows the input and output of this channel:
+   - ChannelData class takes 3-args of:
+      - datapath: string, pointing at '.db' file location 
+      - target_table: string,
+      - channel_Unique_id: string, specific channel ID
+   - Calling corpus() method returns:
+      - the original message content
+
+
+### ---------------------------------------------
+### Data Cleaning 
+
+The data cleaning procedure includes the following steps:
+<ol>
+  <li>Lower case the text corpus.</li>
+  <li>Removing a set of symbols and expressions : $\{\# \,\, n’t\,\,  !\,\,   @\,\,  \,\, ,  “\,\,   "\,\,  ’s\,\,  ()\,\,’ \,\, ? \,\, \}$.</li>
+  <li>Removing one-digit and non-digit characters.</li>
+  <li>Removng mail server and domain of the existing email addresses.</li>
+  <li>Removing URLs.</li>
+  <li>Removing the expression 'rt' if it happens at the beginning of a line.</li>
+  <li>Duplication Removal.</li>
+  <li>Lemmatization</li>
+  <li>Stemming</li>
+</ol>
+The preprocessed data is stored as stripped text in database. 
+
+The pseudocode of preprocessing is presented herein: 
+
+
+<figure>
+<center>
+  <img src="./imgs/dc.png" alt="data_cleaning" style="width:70%">
+  <figcaption>The pseudocode of text preprocessing. </figcaption>
+  </center>
+</figure>
+
+
+
+The following text example presents an example of original message content and stripped text; the test sample is taken from Telegram channel V) Anti-Lockdown Ireland.
+
+<figure>
+<center>
+  <img src="./imgs/dps.png" alt="preprocesing_example" style="width:70%">
+  <figcaption>Sample of data preprocessing from Telegram channel V) Anti-Lockdown Ireland. </figcaption>
+  </center>
+</figure>
+
+
+The customized list of stop-words can be found in 'stopwords.txt' file; The categorized list of stopwords is presented in the following tables: 
+<figure>
+<center>
+  <img src="./imgs/0.png" alt="" style="width:85%">
+  </center>
+</figure>
+
+<figure>
+<center>
+  <img src="./imgs/1.png" alt="" style="width:70%">
+  </center>
+</figure>
+
+<figure>
+<center>
+  <img src="./imgs/2.png" alt="" style="width:70%">
+  </center>
+</figure>
+<figure>
+<center>
+  <img src="./imgs/3.png" alt="" style="width:70%">
+  </center>
+</figure>
+<figure>
+<center>
+  <img src="./imgs/4.png" alt="" style="width:90%">
+  </center>
+</figure>
+<figure>
+<center>
+  <img src="./imgs/5.png" alt="" style="width:90%">
+  </center>
+</figure>
+
+
+### ---------------------------------------------
+### How to Use the data_preprocessing.py  
--- Data-Preprocessing/data_preprocessing.py
+++ Data-Preprocessing/data_preprocessing.py
@@ -1,55 +1,221 @@
+''' --------------------------------------------------------------------- '''
+''' -----------------------  Import Dependencies ------------------------ '''
 
+from sklearn.feature_extraction import text
+from ctypes.wintypes import WORD
 
+import pandas as pd
+import re
+import logging as ping
+from sqlalchemy import create_engine
 
+import nltk
+from nltk.corpus import wordnet
+from nltk.corpus import wordnet
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.porter import *
+from nltk.stem.porter import PorterStemmer
+nltk.download('punkt')
+nltk.download('stopwords')
 
-''' ---------------------------  Channel Data ---------------------------'''
-'''
+''' --------------------------------------------------------------------- '''
+''' ---------------------------  Channel Data --------------------------- '''
+''' There are two separate Tables for each channel of data in our dataset, 
+    These tables are read and appended in one as the orig text corpus. 
+    The object take list of target channel IDs. 
+
     ChannelData class takes 3-args of:
         - datapath: string, pointing at '.db' file location 
-        - target_table: string,
-        - channel_Unique_id: string, specific channel ID
-    Calling corpus() method returns:
-        - the target_channel strip_text data
-        - the original message content
-'''
+        - list_6irish_channels,
+        - target_table_1: str('Target Table_1'),
+        - target_table_2: str('Target Table_2')):
 
+    Calling corpus() method returns:
+        - TextCorpus = the original message content '''
 
 class ChannellData:
 
-    target_channel_strip_text = {}
-    original_message_content = {}
+    TextCorpus = pd.DataFrame()
 
     def __init__(self,
-                 datapath: 'Path to database directory',
-                 target_table: 'Target Table',
-                 channelUniqueId):
+                 datapath: str('Path to database directory'),
+                 list_6irish_channels,
+                 target_table_1: str('Target Table_1'),
+                 target_table_2: str('Target Table_2')):
+        
         self.datapath = datapath
-        self.target_table = target_table
-        self.channelUniqueId = channelUniqueId
-
+        self.list_6irish_channels = list_6irish_channels
+        self.target_table_1 = target_table_1
+        self.target_table_2 = target_table_2
+        
     def corpus(self):
 
         ping.info('Saving recipes to {}'.format(self.datapath))
         db = create_engine('sqlite:///{}'.format(self.datapath))
-
-        target_data_table = pd.read_sql(self.target_table, con=db)
-        target_data_table = target_data_table.loc[target_data_table.semantic_unit_count != ""]
-        target_data_table["semantic_unit_count"] = target_data_table["semantic_unit_count"].astype(
-            int)
-
-        target_ch_table = target_data_table.loc[target_data_table.ChannelUniqueID ==
-                                                self.channelUniqueId]
-
-        text_pool = target_ch_table.strip_text.loc[target_ch_table.semantic_unit_count >= 4].tolist(
-        )
-        self.__class__.target_channel_strip_text = text_pool
-
-        orig_text_pool = target_ch_table.MessageContent.loc[target_ch_table.semantic_unit_count >= 4].tolist(
-        )
-        self.__class__.original_message_content = orig_text_pool
-
-        return self.__class__.target_channel_strip_text, self.__class__.original_message_content
-
-
-
         
+        
+        text_content_1 = pd.read_sql(self.target_table_1,con=db)
+        text_content_1 = text_content_1.loc[text_content_1["Channel_ID"].isin(self.list_6irish_channels)]
+        
+        text_content_2 = pd.read_sql(self.target_table_2,con=db)
+        text_content_2 = text_content_2.loc[text_content_2["Channel_ID"].isin(self.list_6irish_channels)]
+        
+        self.__class__.TextCorpus = text_content_1.append(text_content_2)
+        return self.__class__.TextCorpus
+
+
+''' ---------------------------------------------------------------------- '''
+''' ---------------------------  Data Cleaning  -------------------------- '''
+''' 
+    
+
+
+ '''
+
+
+
+class DataCleaning:
+    
+    datapath = r'newdb.db'
+    db = create_engine('sqlite:///{}'.format(datapath))
+    TextCorpus_unique = pd.DataFrame()
+    lemstem_text_Content = ''
+    len_textcorpus = 0
+
+    def __init__(self, 
+                 orig_corpus: str('Original Corpus in form of DataFrame'), 
+                 output_table: str('Name of output table')) -> None:
+        self.orig_corpus = orig_corpus
+        self.output_table = output_table
+
+    
+    #----------------------------------------
+    @staticmethod
+    def get_wordnet_pos(word):
+
+        # Mapping Part of Speech tag to the first character that lemmatize() accepts
+        tag = nltk.pos_tag([word])[0][1][0].upper()
+        
+        tag_dict = {"J": wordnet.ADJ,
+                    "N": wordnet.NOUN,
+                    "V": wordnet.VERB,
+                    "R": wordnet.ADV,
+                    "S": wordnet.ADJ_SAT}
+        
+        if tag in tag_dict.keys():
+            return tag_dict.get(tag, tag_dict[tag])
+    
+        else:
+            return tag_dict.get(tag, wordnet.NOUN)
+    
+    
+    #----------------------------------------
+    def StemLemmatizerText(self, text_Content):
+        h=list()
+        h[:]=[]
+
+        text_Content= text_Content.lower()
+
+        text_Content = re.sub(r'^https?:\/\/.*[\r\n]*', ' ', text_Content)
+
+        text_Content = re.sub('https?[:;]?/?/?\S*', ' ', text_Content)
+
+        text_Content = re.sub(r'#|!|@|,|•|“|”|\"|\'|’s|n’t|’','',text_Content)
+
+        text_Content = re.sub(r'\?',' ',text_Content)
+
+        text_Content = re.sub('[.] *',' ',text_Content)
+
+        text_Content = re.sub(r"(@\[A-Za-z0-9]+)|([^0-9A-Za-z \t])|[0-9]?[0-9]?(\.[0-9][0-9]?)?|(\w+:\/\/\S+)|^rt", "", text_Content)
+
+        text_Content = re.sub(r'com/(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+[ ]?',' ',text_Content)
+
+
+        for token in [WordNetLemmatizer().lemmatize(w, pos = self.__class__.get_wordnet_pos(w)) for w in nltk.word_tokenize(text_Content)]:
+            if wordnet.morphy(token):
+                token=wordnet.morphy(token)
+
+            if self.__class__.get_wordnet_pos(token)==wordnet.NOUN:
+                 token =  PorterStemmer().stem(token)
+
+            if not token.isdigit() and token not in text.ENGLISH_STOP_WORDS:
+                h.append(WordNetLemmatizer().lemmatize(token, pos = self.__class__.get_wordnet_pos(token)))
+
+        stemed_text_Content=''            
+        stemed_text_Content=' '.join(h)
+        return stemed_text_Content
+    
+    
+    #----------------------------------------
+    def refine_specific_char(self):
+        TextCorpus_unique = self.orig_corpus.drop_duplicates(subset = ['Channel_ID', 'MessageContent', 'MessageID', 'MessageDate', 'Message_FROm_UserID'], keep = 'first')
+        Text_Corpus_Refine = TextCorpus_unique
+        Text_Corpus_Refine = Text_Corpus_Refine.loc[ ~(Text_Corpus_Refine['MessageContent'].str.match(r'https?[:;]?/?/?\S*')) ]
+        Text_Corpus_Refine = Text_Corpus_Refine.loc[ ~(Text_Corpus_Refine['MessageContent'].str.contains("Tnx for joining")) ]
+        Text_Corpus_Refine = Text_Corpus_Refine.loc[ ~(Text_Corpus_Refine['MessageContent'].str.contains("Thanks for joining")) ]
+        Text_Corpus_Refine = Text_Corpus_Refine.loc[ ~(Text_Corpus_Refine['MessageContent'].str.contains("Thank you to all the new members for joining")) ]
+        Text_Corpus_Refine = Text_Corpus_Refine.loc[ ~(Text_Corpus_Refine['MessageContent'].str.contains("Thank u x")) ]
+        Text_Corpus_Refine = Text_Corpus_Refine.loc[ ~(Text_Corpus_Refine['MessageContent'].str.contains("Thanks for joining our small group")) ]
+        Text_Corpus_Refine = Text_Corpus_Refine.loc[ ~(Text_Corpus_Refine['MessageContent'].str.contains("Thanks for Joining")) ]
+        Text_Corpus_Refine = Text_Corpus_Refine.loc[ ~(Text_Corpus_Refine['MessageContent'].str.contains(" has been banned! Reason: CAS ban")) ]
+        Text_Corpus_Refine = Text_Corpus_Refine.loc[ ~(Text_Corpus_Refine['MessageContent'].str.contains("SEND OUR QR CODE")) ]
+        Text_Corpus_Refine = Text_Corpus_Refine.loc[ ~(Text_Corpus_Refine['MessageContent'].str.contains("SHARE OUR QR CODE")) ]
+        Text_Corpus_Refine = Text_Corpus_Refine.loc[ ~(Text_Corpus_Refine['MessageContent'].str.contains("send our QR code")) ]
+        Text_Corpus_Refine = Text_Corpus_Refine.loc[ ~(Text_Corpus_Refine['MessageContent'].str.match("Check this out.."))]
+        self.__class__.TextCorpus_unique = Text_Corpus_Refine.reset_index()
+        self.__class__.len_textcorpus = len(Text_Corpus_Refine)
+
+
+    #----------------------------------------
+    def lemma_stem(self):
+        step = 10000
+        strt = 0
+        end = strt + step
+        df = self.__class__.TextCorpus_unique
+        
+        while end < self.__class__.len_textcorpus:
+    
+            temp_df = df.loc[strt:end]
+            temp_df["strip_text"] = ''
+            temp_df["semantic_unit_count"] = ''
+
+            for i in range(strt, end+1):
+                if temp_df.at[i,"MessageContent"] and self.__class__.StemLemmatizerText(self, temp_df.at[i,"MessageContent"]):
+                    temp_df.at[i,"strip_text"] = self.__class__.StemLemmatizerText(self,temp_df.at[i,"MessageContent"])
+                    temp_df.at[i,"semantic_unit_count"] = len(nltk.word_tokenize(temp_df.at[i,"strip_text"]))
+    
+            temp_df.to_sql(self.output_table,
+                           con=self.__class__.db, 
+                           index=False, 
+                           if_exists="replace")
+    
+            strt = end + 1
+            end  = end + step    
+            del(temp_df)
+
+''' ------------------------------------ '''    
+''' --------------- Main --------------- '''
+def main():
+    lst_ich = [-1001250192398,
+               -1001486710610,
+               -1001719423078, 
+               -1001522159637,
+               -1001242115487,
+               -1001198445854]
+    
+    chd = ChannellData(datapath = r'TelegramData.db',
+                        list_6irish_channels = lst_ich,
+                        target_table_1 = "TextContent_Telegram_Channel",
+                        target_table_2 = "TextContent_Telegram_Channel_2")
+
+    orig_msg_content = chd.corpus()
+
+    # create data cleaning object
+    dco = DataCleaning(orig_msg_content, output_table = "Strip_Text")
+    
+    dco.lemma_stem()
+    
+
+''' ---------------------------------------'''
+if __name__ == "__main__":
+    main()
